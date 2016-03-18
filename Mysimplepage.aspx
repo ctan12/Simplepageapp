03acd6ec17953cddab11bae402d5069fef7bf206
ctan12 <tania.chtt@gmail.com>
2016-03-17 22:33:22 -0700

    Added files via upload

---
+++ Mysimplepage.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Mysimplepage.aspx.cs" Inherits="WebApplication1.Mysimplepage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <div>
+      
+
+    <form id="form1" runat="server" method="post" >
+      
+    
+        Product ID:<br />
+        <input type="text" name="ProductID" value="" /><br />
+      
+
+        
+ Product Name:<input type="text" name="ProductName" value="" style="width: 574px; height: 32px" /><br />
+        <br />
+
+          Product Details:<input type="text" name="ProductDetails" value="" style="width: 643px; height: 56px" /><br />
+        <br /><br />
+<%-- <input type="submit" value="Submit" class="submit" />
+ --%>
+<h3> Enter Product File to Upload:</h3>
+         <asp:FileUpload ID="FileUpload1" runat="server" />
+         <br />
+         <br /><br />
+         <asp:Button ID="btnsave" runat="server" onclick="btn_save"  Text="Submit" style="width:85px" />
+         <br /><br />
+         <asp:Label ID="lblmessage" runat="server" />
+
+      <%--<button type="button" class="btn btn-default btn-sm">
+        <span class="glyphicon glyphicon-star-empty"></span> Submit
+    </button>--%>
+   
+    
+
+    </form>
+
+
+     </div>
+</body>
+</html>
---
+++ Mysimplepage.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class Mysimplepage : System.Web.UI.Page
+    {
+        protected void btn_save(object sender, EventArgs e)
+        {
+            System.Text.StringBuilder sb = new StringBuilder();
+
+            if (Page.IsValid)
+            {
+                Response.Redirect("~/Thankyou.html");
+
+            }
+            else
+            {
+                Response.Redirect("~/Fillup.html");
+
+            }
+
+
+
+
+            if (FileUpload1.HasFile)
+            {
+
+                try
+                {
+                    sb.AppendFormat(" Uploading file: {0}", FileUpload1.FileName);
+
+                    //saving the file
+                    FileUpload1.SaveAs("<c:\\SaveDirectory>" + FileUpload1.FileName);
+
+                    //Showing the file information
+                    sb.AppendFormat("<br/> Save As: {0}", FileUpload1.PostedFile.FileName);
+                    sb.AppendFormat("<br/> File type: {0}", FileUpload1.PostedFile.ContentType);
+                    sb.AppendFormat("<br/> File length: {0}", FileUpload1.PostedFile.ContentLength);
+                    sb.AppendFormat("<br/> File name: {0}", FileUpload1.PostedFile.FileName);
+
+                }
+                catch (Exception ex)
+                {
+                    sb.Append("<br/> Error <br/>");
+                    sb.AppendFormat("Unable to save file <br/> {0}", ex.Message);
+                }
+            }
+            else
+            {
+                lblmessage.Text = sb.ToString();
+            }
+
+        }
+
+    }
+}
